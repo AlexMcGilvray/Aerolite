2b70052f9606e7626d98a2e1f1efe494b3238ede
Alex McGilvray <alex@levelism.com>
2017-02-27 21:06:18 -0800

    fastest implementation ever of a curtains system. It leaves a lot to the imagination but it works which is more than I can say for most implementations. There is a single function called ChangeStateWithCurtains that fades out, changes state and then fades in again

--- Aerolite/Aerolite/Subsystems/AeStateManager.cs
+++ Aerolite/Aerolite/Subsystems/AeStateManager.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Xna.Framework;
+﻿using Aerolite.Entity;
+using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using System;
 using System.Collections.Generic;
@@ -8,6 +9,93 @@ using System.Threading.Tasks;
 
 namespace Aerolite.Subsystems
 {
+    public enum FadingDirection
+    {
+        NotFading,
+        In,
+        Out
+    }
+
+    public class AeCurtainManager : AeEntity
+    {
+        public FadingDirection FadeDirection { get; private set; }
+        Texture2D _curtainsTexture;
+        float CurrentAlpha { get; set; }
+        bool IsActive { get; set; }
+
+        Action _fadeInCallback;
+        Action _fadeOutCallback;
+
+        Rectangle _screenRect;
+        public AeCurtainManager()
+        {
+            _curtainsTexture = Engine.TextureManager.CreateFilledRectangle(1, 1, Color.Black);
+        }
+
+        public void FadeIn(Action callback = null)
+        {
+            CurrentAlpha = 1.0f;
+            FadeDirection = FadingDirection.In;
+                _fadeInCallback = callback;
+        }
+
+        public void FadeOut(Action callback = null)
+        {
+            CurrentAlpha = 0;
+            FadeDirection = FadingDirection.Out;
+            _fadeOutCallback = callback;
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            base.Update(gameTime);
+            if (FadeDirection == FadingDirection.In)
+            {
+                if (CurrentAlpha > 0)
+                {
+                    CurrentAlpha -= 0.1f;
+                }
+                else
+                {
+                    if (_fadeInCallback != null)
+                    {
+                        _fadeInCallback();
+                        _fadeInCallback = null;
+                    }
+                    FadeDirection = FadingDirection.NotFading;
+                }
+            }
+            else if (FadeDirection == FadingDirection.Out)
+            {
+                if (CurrentAlpha < 1.0f)
+                {
+                    CurrentAlpha += 0.1f;
+                }
+                else
+                {
+                    if (_fadeOutCallback != null)
+                    {
+                        _fadeOutCallback();
+                        _fadeOutCallback = null;
+                    }
+                    FadeDirection = FadingDirection.NotFading;
+                }
+            }
+        }
+
+        public override void Draw(GameTime gameTime, SpriteBatch batch)
+        {
+            base.Draw(gameTime, batch);
+            _screenRect.X = 0;
+            _screenRect.Y = 0;
+            _screenRect.Width = Engine.Graphics.GraphicsSettings.GameResolutionWidth;
+            _screenRect.Height = Engine.Graphics.GraphicsSettings.GameResolutionHeight;
+
+            batch.Draw(_curtainsTexture, _screenRect, Color.White * CurrentAlpha);
+        }
+    }
+
+
     public class AeStateManager
     {
         private List<AeState> _states = new List<AeState>();
@@ -17,6 +105,8 @@ namespace Aerolite.Subsystems
         /// we're iterating on it. So we defer all state manipulations until after all state updates are complete. 
         /// </summary>
         private List<Action> _stateManiulationDeferredOperations = new List<Action>();
+
+        private AeCurtainManager _curtains = new AeCurtainManager();
 
         public AeStateManager()
         { }
@@ -58,11 +148,28 @@ namespace Aerolite.Subsystems
             });
         }
 
-
+        public void ChangeStateWithCurtains(AeState state)
+        {
+            _stateManiulationDeferredOperations.Add(() =>
+            {
+                _curtains.FadeOut(() => 
+                {
+                    _stateManiulationDeferredOperations.Add(() =>
+                    {
+                        _states.Clear();
+                        _states.Add(state);
+                        _curtains.FadeIn();
+                    });
+                });
+            });
+        }
 
         public void Update(GameTime gameTime)
         {
-
+            if (_curtains.FadeDirection != FadingDirection.NotFading)
+            {
+                _curtains.Update(gameTime);
+            }
             //states shouldn't bleed into each other which technically means 
             //I might be able to do this with a parallel for....
             for (int i = _states.Count - 1; i >= 0; --i)
@@ -81,6 +188,12 @@ namespace Aerolite.Subsystems
             for (int i = 0; i < _states.Count; ++i)
             {
                 _states[i].Draw(gameTime, spriteBatch);
+            }
+            if (_curtains.FadeDirection != FadingDirection.NotFading)
+            {
+                spriteBatch.Begin(SpriteSortMode.Deferred, BlendState.AlphaBlend, SamplerState.PointClamp, DepthStencilState.Default, RasterizerState.CullNone);
+                _curtains.Draw(gameTime, spriteBatch);
+                spriteBatch.End();
             }
         }
     }

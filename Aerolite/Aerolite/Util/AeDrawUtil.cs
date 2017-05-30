a96112b481be7c517e8281f034e1e126597a37f4
Alex McGilvray <alex@levelism.com>
2017-05-29 21:10:35 -0700

    implemented rectangle drawing utility function

--- Aerolite/Aerolite/Util/AeDrawUtil.cs
+++ Aerolite/Aerolite/Util/AeDrawUtil.cs
@@ -10,11 +10,41 @@ namespace Aerolite.Util
 {
     public static class AeDrawUtil
     {
-        public static void DrawHollowRectangle(SpriteBatch batch, Rectangle rect, int border)
+        public static void DrawHollowRectangle(SpriteBatch batch, Rectangle sourceRect, int border = 1)
         {
-            throw new NotImplementedException();
+            DrawHollowRectangle(batch, sourceRect, Color.White, border);
         }
 
-        
+        public static void DrawHollowRectangle(SpriteBatch batch, Rectangle sourceRect, Color color ,int border = 1)
+        {
+            Texture2D fill = AeEngine.Singleton().TextureManager.GetFillTexture();
+
+            Rectangle rect;
+            rect.X = sourceRect.X;
+            rect.Y = sourceRect.Y;
+            rect.Width = sourceRect.Width;
+            rect.Height = border;
+            batch.Draw(fill, rect, color);
+
+            rect.X = sourceRect.X;
+            rect.Y = (sourceRect.Y + sourceRect.Height - 1);
+            rect.Width = sourceRect.Width;
+            rect.Height = border;
+            batch.Draw(fill, rect, color);
+
+            rect.X = sourceRect.X;
+            rect.Y = sourceRect.Y;
+            rect.Width = border;
+            rect.Height = sourceRect.Height;
+            batch.Draw(fill, rect, color);
+
+            rect.X = sourceRect.X + sourceRect.Width;
+            rect.Y = sourceRect.Y;
+            rect.Width = border;
+            rect.Height = sourceRect.Height;
+            batch.Draw(fill, rect, color);
+        }
+
+
     }
 }
